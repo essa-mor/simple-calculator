ca54c04741864a5dd802b7769171002bcb0c8ab3
issa-mour <issa.mour2@gmail.com>
2018-09-04 14:45:53 +0300

    create the layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React</h1>
-				</header>
-				<p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
+				<input type="text" value="0" className="result_input" readOnly/>
+				<button type="button" className="clear_result">Clear</button>
+				<button type="button" className="operator">-</button>
+				<button type="button" className="digit">7</button>
+				<button type="button" className="digit">8</button>
+				<button type="button" className="digit">9</button>
+				<button type="button" className="operator">/</button>
+				<button type="button" className="digit">4</button>
+				<button type="button" className="digit">5</button>
+				<button type="button" className="digit">6</button>
+				<button type="button" className="operator">*</button>
+				<button type="button" className="digit">1</button>
+				<button type="button" className="digit">2</button>
+				<button type="button" className="digit">3</button>
+				<button type="button" className="operator">+</button>
+				<button type="button" className="digit">0</button>
+				<button type="button" className="digit">.</button>
+				<button type="button" className="equal">=</button>
 			</div>
 		);
 	}
