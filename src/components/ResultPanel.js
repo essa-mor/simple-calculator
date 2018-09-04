49e3b740df3e2ea5fe0597e4ac30193261e4d373
issa-mour <issa.mour2@gmail.com>
2018-09-04 19:08:13 +0300

    perform operation

---
+++ src/components/ResultPanel.js
@@ -0,0 +1,31 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+import './ResultPanel.css';
+
+class ResultPanel extends PureComponent {
+	constructor(props) {
+		super(props);
+		this.textInput = React.createRef();
+	}
+
+	componentDidMount() {
+		this.textInput.current.focus();
+	}
+
+	render() {
+		const { summary, result, className } = this.props;
+		return (<div className={className}>
+			<span className="result_label">{summary}</span>
+			<input type="text" ref={this.textInput} value={result} className="result_input" readOnly />
+		</div>);
+	}
+}
+
+ResultPanel.propTypes = {
+	summary: PropTypes.string, 
+	result: PropTypes.number, 
+	className: PropTypes.string
+};
+
+export default ResultPanel;
